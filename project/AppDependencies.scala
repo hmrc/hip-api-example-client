7052ebb86b2ea26f9b98633f7b457ade78b67092
Victor Arbues <painyjames@gmail.com>
2024-08-15 15:24:34 +0100

    HIPP-1322: Migrate to Scala 3 (#8)
    * HIPP-1322: Migrate to Scala 3
    
    * HIPP-1322: Address PR bot comments

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -2,8 +2,8 @@ import sbt._
 
 object AppDependencies {
 
-  private val bootstrapVersion = "8.4.0"
-  private val internalAuthVersion = "1.9.0"
+  private val bootstrapVersion = "9.0.0"
+  private val internalAuthVersion = "3.0.0"
 
   val compile = Seq(
     "uk.gov.hmrc"             %% "bootstrap-backend-play-30"    % bootstrapVersion,
@@ -12,7 +12,6 @@ object AppDependencies {
 
   val test = Seq(
     "uk.gov.hmrc"             %% "bootstrap-test-play-30"     % bootstrapVersion            % Test,
-    "org.mockito"             %% "mockito-scala"              % "1.17.30"                   % Test
   )
 
   val it = Seq.empty
